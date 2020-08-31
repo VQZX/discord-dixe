086f59667cc7b50633a23bfb9824e9845546ae54
Tim Flusk <tpflusk@gmail.com>
2020-08-31 04:45:59 +0200

    Added troika tables

---
+++ commands/troika/troika-weapon.js
@@ -0,0 +1,30 @@
+
+const graf = require('discord-graf');
+const diceUtility = require('dice-utility')
+
+module.exports = class TroikaWeaponRoll extends graf.Command
+{
+    constructor(bot)
+    {
+        super
+        (
+            bot,
+            {
+                name: 'troika-weapon',
+                aliases: ['troika-table', 'troika-weapon'],
+                module: 'troika',
+                memberName: 'troika-weapon',
+                description: 'A bot for handling the mapping of weapon tables of the TTRPG Troika',
+                usage: 'troika-table <table> <roll>',
+                pattern: ''
+            }
+        );
+    }
+
+    run(message, args)
+    {
+        const result = diceUtility.rollTroikaTable(args[0], null);
+        const output = `threw a ***${result.weapon}*** with ***${result.result}*** dmg`;
+        return Promise.resolve(output);
+    }
+}
--- dixe.js
+++ dixe.js
@@ -10,6 +10,7 @@ const rollCommand = require('./commands/basic/roll');
 const rollfakeCommand = require('./commands/basic/roll-fake');
 const maxCommand = require('./commands/basic/max');
 const minCommand = require('./commands/basic/min');
+const troikaCommand = require('./commands/troika/troika-weapon');
 
 // Alright alright, the above compiles
 const version = '0.0.1';
@@ -20,8 +21,8 @@ const dixeBot = new graf.Bot
         name: 'Dixe-Bot',
         version: version,
         token: config.token,
-        email: 'youcantgetmyemailaddress@copper.com',
-        password: 'thisisapasswordipromise',
+        email: config.email,
+        password: config.password,
         clientOptions:
             {
                 disable_everyone: true
@@ -34,10 +35,11 @@ const dixeBot = new graf.Bot
 // Add some commands
 const modules = [
     ['basic', 'Basic'],
-    ['test', 'Test']
+    ['test', 'Test'],
+    ['troika', 'Troika']
 ];
 
-const commands = [rollfakeCommand, debugCommand, minCommand, maxCommand, rollCommand];
+const commands = [rollfakeCommand, debugCommand, minCommand, maxCommand, rollCommand, troikaCommand];
 const client = dixeBot.
     registerDefaults().
     registerModules(modules).
